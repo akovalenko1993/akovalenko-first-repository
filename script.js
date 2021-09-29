fbe0ea4b2a04bcf1dfc8f59efd378d9977b77a6c
akovalenko1993 <alex.kovalenko.1993@gmail.com>
2021-09-29 17:36:53 +0300

    пробую обратные кавычки`

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-let admin;
-let name;
-name = 'Джон';
-admin = name;
-alert(admin);
+//
+//let admin;
+//let name;
+//name = 'Джон';
+//admin = name;
+//alert(admin);
+//
+
+let nameOfPlanet;
+nameOfPlanet = 'Earth';
+alert(nameOfPlanet);
+
+let name = 'Oleksii';
+alert(`Hello ${1}`);
+alert(`Hello ${"name"}`);
+alert(`Hello ${name}`);
+
