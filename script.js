3b8e6c68181a9f790fec4e2d9737232294223cee
akovalenko1993 <alex.kovalenko.1993@gmail.com>
2021-09-29 18:43:38 +0300

    пробую prompt

--- script.js
+++ script.js
@@ -6,12 +6,23 @@
 //alert(admin);
 //
 
-let nameOfPlanet;
-nameOfPlanet = 'Earth';
-alert(nameOfPlanet);
+//let nameOfPlanet;
+//nameOfPlanet = 'Earth';
+//alert(nameOfPlanet);
 
-let name = 'Oleksii';
-alert(`Hello ${1}`);
-alert(`Hello ${"name"}`);
-alert(`Hello ${name}`);
+//let name = 'Oleksii';
+//alert(`Hello ${1}`);
+//alert(`Hello ${"name"}`);
+//alert(`Hello ${name}`);
+
+
+
+//userAge = prompt('howOldAreYou', []);
+//alert(userAge);
+
+let age = prompt('сколько тебе лет', []);
+alert(`тебе ${age} лет`);
+let halfAge;
+halfAge = age / 2;
+alert(halfAge);
 
