7960e9919b55b5d1f16777557caf887778675094
akovalenko1993 <alex.kovalenko.1993@gmail.com>
2021-09-29 18:54:55 +0300

    сделал задачу

--- script.js
+++ script.js
@@ -20,9 +20,12 @@
 //userAge = prompt('howOldAreYou', []);
 //alert(userAge);
 
-let age = prompt('сколько тебе лет', []);
-alert(`тебе ${age} лет`);
-let halfAge;
-halfAge = age / 2;
-alert(halfAge);
+//let age = prompt('сколько тебе лет', []);
+//alert(`тебе ${age} лет`);
+//let halfAge;
+//halfAge = age / 2;
+//alert(halfAge);
 
+let userName = prompt('как тебя зовут', []);
+let userSecondName = prompt('как твоя фамилия', []);
+alert(`твое имя ${userName}, твоя фамилия ${userSecondName}`);
